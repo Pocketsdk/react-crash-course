4c490cdf185658c34959e72e60af0092c2b76e0e
PocketsDK <pocketsdk@outlook.com>
2024-10-07 16:11:09 -0600

    Removed unneeded import from App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,6 @@ import Navbar from './components/navbar'
 import Hero from './components/Hero'
 import HomeCards from './components/HomeCards'
 import JobListings from './components/JobListings'
-import VeiwAllJobs from './components/ViewAllJobs'
 import ViewAllJobs from './components/ViewAllJobs'
 
 const App = () => {
