010c54c0c8e40df4786b1b30568a66afeeab8077
PocketsDK <pocketsdk@outlook.com>
2024-10-09 08:42:48 -0600

    Formatted with prettier and added state to the job cards to show more or less text in the description.

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,20 @@
-import React from 'react'
-import Navbar from './components/navbar'
-import Hero from './components/Hero'
-import HomeCards from './components/HomeCards'
-import JobListings from './components/JobListings'
-import ViewAllJobs from './components/ViewAllJobs'
+import React from "react";
+import Navbar from "./components/Navbar";
+import Hero from "./components/Hero";
+import HomeCards from "./components/HomeCards";
+import JobListings from "./components/JobListings";
+import ViewAllJobs from "./components/ViewAllJobs";
 
 const App = () => {
   return (
     <>
-        <Navbar />
-        <Hero />
-        <HomeCards />
-        <JobListings />
-        <ViewAllJobs />
-
+      <Navbar />
+      <Hero />
+      <HomeCards />
+      <JobListings />
+      <ViewAllJobs />
     </>
-  )
-}
+  );
+};
 
-export default App
+export default App;
