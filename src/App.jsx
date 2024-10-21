07a23dbc3c0c96505f395fdb6546bf5e72cc7da2
PocketsDK <pocketsdk@outlook.com>
2024-10-21 10:35:06 -0600

    added job details page (basic) and fetching job by id using the data loader

--- src/App.jsx
+++ src/App.jsx
@@ -10,12 +10,14 @@ import HomePage from "./Pages/HomePage";
 import JobsPage from "./Pages/JobsPage";
 import AddJob from "./Pages/AddJob";
 import NotFoundPage from "./Pages/NotFoundPage";
+import JobPage, { jobLoader } from "./Pages/JobPage";
 
 const router = createBrowserRouter(
   createRoutesFromElements(
     <Route path="/" element={<MainLayout />}>
       <Route index element={<HomePage />} />
       <Route path="/jobs" element={<JobsPage />} />
+      <Route path="/jobs/:id" element={<JobPage />} loader={jobLoader} />
       {/* <Route path="/add-job" element={<AddJob />} /> */}
       <Route path="*" element={<NotFoundPage />} />
     </Route>
