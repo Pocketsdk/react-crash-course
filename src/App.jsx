78d5dbcf2a6fc6b2f2561d109d85c6e9d45b2803
PocketsDK <pocketsdk@outlook.com>
2024-10-29 09:40:47 -0600

    Added edit job functions

--- src/App.jsx
+++ src/App.jsx
@@ -34,6 +34,18 @@ const App = () => {
     return;
   };
 
+  // Update job
+  const updateJob = async (job) => {
+    const res = await fetch(`/api/jobs/${job.id}`, {
+      method: "PUT",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(job),
+    });
+    return;
+  };
+
   const router = createBrowserRouter(
     createRoutesFromElements(
       <Route path="/" element={<MainLayout />}>
@@ -42,7 +54,7 @@ const App = () => {
         <Route path="/add-job" element={<AddJobPage addJobSubmit={addJob} />} />
         <Route
           path="/edit-job/:id"
-          element={<EditJobPage />}
+          element={<EditJobPage updateJobSubmit={updateJob} />}
           loader={jobLoader}
         />
         <Route
